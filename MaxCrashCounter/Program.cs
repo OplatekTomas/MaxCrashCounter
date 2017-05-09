3d09abe75848f74b63598ac78359925a0f537347
Kaharonus <oplimail@gmail.com>
2017-05-09 13:38:40 +0200

    Yep I'm bored. Bug fix

--- MaxCrashCounter/Program.cs
+++ MaxCrashCounter/Program.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MaxCrashCounter {
     class Program {
         static void Main(string[] args) {
             int crashes = ReadCrash();
-            Console.WriteLine("Number of crashes so far: " + crashes);
+            Console.WriteLine("Number of crashes so far: " + crashes + "\nWaiting for another crash...");
             while (true) {
                 List<Process> proc = Process.GetProcessesByName("senddmp").ToList();
                 if (proc.Count > 0) {
                     crashes++;
-                    proc[0].Kill();
+                    foreach (Process pr in proc) {
+                        pr.Kill();
+                    }
                     Console.WriteLine("Yep it crashed. Crash number:" + crashes);
-                    string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MaxCrashCount.txt";
+                    string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MaxCrashCount.Kusak";
                     File.WriteAllText(path, crashes.ToString());
-                    
+                    Console.WriteLine("Waiting for another crash...");
+
                 }
                 Thread.Sleep(1000);
             }
@@ -28,7 +29,7 @@ namespace MaxCrashCounter {
 
 
         private static int ReadCrash() {
-            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MaxCrashCount.txt";
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MaxCrashCount.Kusak";
             if (File.Exists(path)) {
                 return Int32.Parse(File.ReadAllText(path));
             }
